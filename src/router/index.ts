1dcb17d9c305138df6a0bd71df9b3eb0abe75114
ControlNet <smczx@hotmail.com>
2023-01-15 02:01:36 +1100

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import WelcomeView from "@/views/WelcomeView.vue";
 import MainView from "@/views/MainView.vue";
 
 export default createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory("/"),
     routes: [
         { path: "/", component: WelcomeView},
         { path: "/:mode/:videoId", component: MainView},
