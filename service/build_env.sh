6d66034a9849f404324e2ca9a6b0fc56ae6efb0c
ControlNet <smczx@hotmail.com>
2023-08-31 22:58:46 +1000

    update deps

--- service/build_env.sh
+++ service/build_env.sh
@@ -11,5 +11,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
 pip install tensorflow==2.9.1
 pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install pyzmq==24.0.1 tensorneko==0.2.7 transformers==4.24.0 hsemotion==0.2 facenet_pytorch==2.5.2 tensorflow_hub==0.12.0 pydub==0.25.1 librosa==0.9.2 BeautifulSoup4==4.11.1 moviepy==1.0.3 lxml==4.9.1 pandas==1.3.5 fastapi==0.89.0 starlette==0.22.0 uvicorn==0.20.0 websockets==10.4 python-multipart==0.0.5 timm==0.6.13
+pip install pyzmq==24.0.1 tensorneko==0.2.7 transformers==4.24.0 hsemotion==0.2 facenet_pytorch==2.5.2 tensorflow_hub==0.12.0 pydub==0.25.1 librosa==0.9.2 BeautifulSoup4==4.11.1 moviepy==1.0.3 lxml==4.9.1 pandas==1.3.5 fastapi==0.89.0 starlette==0.22.0 uvicorn==0.20.0 websockets==10.4 python-multipart==0.0.5 timm==0.6.13 torchmetrics==0.11.4
 pip install git+https://github.com/openai/whisper.git
