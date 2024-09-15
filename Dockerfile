da896f4513c8824868ccc7e9698c1c012833cb0f
ControlNet <smczx@hotmail.com>
2024-09-15 23:12:26 +1000

    fix CI

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,11 @@ WORKDIR /app
 
 RUN apt update
 RUN apt install -y ffmpeg git
-RUN pip install --upgrade pip
+RUN pip install "pip<24.1"
 RUN /opt/conda/bin/conda init bash
 RUN pip install tensorflow==2.9.1
-RUN pip install torchvision==0.14.1 torchaudio==0.13.1 torch==1.13.1 pyzmq==24.0.1 tensorneko==0.2.7 transformers==4.24.0 hsemotion==0.2 facenet_pytorch==2.5.2 tensorflow_hub==0.12.0 pydub==0.25.1 librosa==0.9.2 BeautifulSoup4==4.11.1 moviepy==1.0.3 lxml==4.9.1 pandas==1.3.5 fastapi==0.89.0 starlette==0.22.0 uvicorn==0.20.0 websockets==10.4 python-multipart==0.0.5 timm==0.6.13 torchmetrics==0.11.4
-RUN pip install git+https://github.com/openai/whisper.git
+RUN pip install torchvision==0.14.1 torchaudio==0.13.1 torch==1.13.1 numpy==1.26.4 numba==0.60.0 pyzmq==24.0.1 tensorneko==0.2.7 transformers==4.24.0 hsemotion==0.3 facenet_pytorch==2.5.2 tensorflow_hub==0.12.0 pydub==0.25.1 librosa==0.9.2 BeautifulSoup4==4.11.1 moviepy==1.0.3 lxml==4.9.1 pandas==1.3.5 fastapi==0.89.0 starlette==0.22.0 uvicorn==0.20.0 websockets==10.4 python-multipart==0.0.5 timm==0.9.7 torchmetrics==0.11.4
+RUN pip install openai_whisper==20231117
 
 COPY . .
 
--- service/build_env.sh
+++ service/build_env.sh
@@ -9,7 +9,14 @@ conda install -y ffmpeg
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
+pip install "pip<24.1"
 pip install tensorflow==2.9.1
 pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install pyzmq==24.0.1 tensorneko==0.2.7 transformers==4.24.0 hsemotion==0.2 facenet_pytorch==2.5.2 tensorflow_hub==0.12.0 pydub==0.25.1 librosa==0.9.2 BeautifulSoup4==4.11.1 moviepy==1.0.3 lxml==4.9.1 pandas==1.3.5 fastapi==0.89.0 starlette==0.22.0 uvicorn==0.20.0 websockets==10.4 python-multipart==0.0.5 timm==0.6.13 torchmetrics==0.11.4
-pip install git+https://github.com/openai/whisper.git
+pip install numpy==1.26.4 numba==0.60.0 pyzmq==24.0.1 scipy==1.13.1 tensorneko==0.2.7 transformers==4.24.0 hsemotion==0.3 facenet_pytorch==2.5.2 tensorflow_hub==0.12.0 pydub==0.25.1 librosa==0.9.2 BeautifulSoup4==4.11.1 moviepy==1.0.3 lxml==4.9.1 pandas==1.3.5 fastapi==0.89.0 starlette==0.22.0 uvicorn==0.20.0 websockets==10.4 python-multipart==0.0.5 timm==0.9.7 torchmetrics==0.11.4
+pip install openai_whisper==20231117
+
+# download models
+mkdir -p checkpoints
+wget https://github.com/ControlNet/emolysis/releases/download/misc/audio_model_trill.pt -O checkpoints/audio_model_trill.pt
+wget https://github.com/ControlNet/emolysis/releases/download/misc/linguistic_head_en.ckpt -O checkpoints/linguistic_head_en.ckpt
+wget https://github.com/ControlNet/emolysis/releases/download/misc/linguistic_head_zh.ckpt -O checkpoints/linguistic_head_zh.ckpt
